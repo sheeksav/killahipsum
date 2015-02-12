f37c9cdc5f0e3e9d56f7fd5d80c6d0452e334a7e
sheeksav <andy.sheeks@gmail.com>
2015-02-12 15:00:38 -0500

    Returning some paragraph text in the view.

--- ipsum/admin.py
+++ ipsum/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Paragraph
+
 # Register your models here.
+admin.site.register(Paragraph)
--- ipsum/views.py
+++ ipsum/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from .models import Paragraph
+
 # Create your views here.
 
 
 class IpsumHomeView(TemplateView):
     template_name = 'ipsum/base.html'
+
+    def get_context_data(self, **kwargs):
+
+        paragraphs = Paragraph.objects.all()
+
+        context = {
+            'paragraphs': paragraphs,
+        }
+
+        return context
